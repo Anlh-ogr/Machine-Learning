2b7bd2fbd451a9923111b27f836523120ed641fc
Anlh-ogr <9.10hoangan.13072003@gmail.com>
2024-09-01 09:53:50 +0700

    Update Diagram_of_Minimization_with_Gradient_Descent.py

--- B3/Diagram_of_Minimization_with_Gradient_Descent.py
+++ B3/Diagram_of_Minimization_with_Gradient_Descent.py
@@ -1,11 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def fx_Function(x):
+def grad(x):
     return x**2 + 6*np.sin(x)
 
 x_val = np.linspace(-4, 4, 1000)
-y_val = fx_Function(x_val)
+y_val = grad(x_val)
 
 plt.plot(x_val, y_val, label='f(x) = x^2 + 6sin(x)')
 plt.title('f(x) = x^2 + 6sin(x)')
